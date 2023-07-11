f834d1af609169c984a4be7d6a56e1253d791fa2
Cao Dang Tinh <caodangtinh04022003@gmail.com>
2023-07-11 21:19:41 +0700

    add blue.css in Image Lazy load

--- src/components/GlobalComponents/Img/index.js
+++ src/components/GlobalComponents/Img/index.js
@@ -1,4 +1,5 @@
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 function Img({ alt, height, src, width, className = '' }) {
   console.log(className);
